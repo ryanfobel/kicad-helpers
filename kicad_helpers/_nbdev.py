27e5d08ef1b7347b792a6dd88db9770e04bab02f
Ryan Fobel <ryan@fobel.net>
2021-11-04 16:56:59 -0400

    Add `init_github_workflows` function

--- kicad_helpers/_nbdev.py
+++ kicad_helpers/_nbdev.py
@@ -2,9 +2,13 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"say_hello": "00_core.ipynb"}
+index = {"get_project_root": "00_core.ipynb",
+         "get_project_name": "00_core.ipynb",
+         "say_hello": "00_core.ipynb",
+         "init_github_workflows": "01_initialize_a_new_project.ipynb"}
 
-modules = ["core.py"]
+modules = ["core.py",
+           "init_project.py"]
 
 doc_url = "https://ryanfobel.github.io/kicad_helpers/"
 
