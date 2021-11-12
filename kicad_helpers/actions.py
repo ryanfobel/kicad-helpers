3fffdc302299e9b43df072bee4e6a79fb79b6375
Ryan Fobel <ryan@fobel.net>
2021-11-12 10:19:29 -0500

    Update docs

--- kicad_helpers/actions.py
+++ kicad_helpers/actions.py
@@ -21,8 +21,13 @@ def update_templates(v:Param("verbose", bool),
                      overwrite:Param("overwrite existing templates", bool),
                      root:Param("project root directory", str)="."):
     """
-    Update project templates from the `kicad_helpers/templates` directory
-    (ignoring anything in the project's `.gitignore` list).
+    Install various templates from the `kicad_helpers/templates` directory (ignoring anything in the project's
+    `.gitignore` list). Similar to `nbdev`: [nbdev_new](https://nbdev.fast.ai/tutorial.html#Set-up-Repo). Templates
+    are stored in the [kicad_helpers/templates](https://github.com/ryanfobel/kicad-helpers/tree/main/kicad_helpers/templates)
+    folder, and are included with the python package by adding the followingline to the `MANIFEST.in` file:
+    ```
+    graft kicad_helpers/templates
+    ```
     """
     templates_path = os.path.abspath(pkg_resources.resource_filename('kicad_helpers', 'templates'))
     root = _set_root(root)
@@ -72,6 +77,7 @@ def sch_to_bom(root:Param("project root directory", str)=".",
         print(cmd)
     subprocess.check_output(cmd, shell=True)
 
+# Cell
 @call_parse
 def bom_to_sch(root:Param("project root directory", str)=".",
                v:Param("verbose", bool)=False,
